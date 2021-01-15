015a3d669d06b26065bf4fce01d87ee02656bdf9
Max Miller <maxmiller413@gmail.com>
2021-01-15 18:19:19 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -11,6 +11,7 @@ def join_nested_strings(src)
       
       if src[row_index][element_index].class == String
         total_strings += src[row_index][element_index] + " "
+      end 
       
       element_index += 1 
     end 
